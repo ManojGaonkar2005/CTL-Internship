99fc3e90a0d98f17ee5b70179e134d71bd77011b
ManojGaonkar2005 <manojgaonkar2005h@gmail.com>
2025-01-29 21:49:33 +0530

    Update in Codes

--- src/Componant/Inspect Car/InspectCar.stories.jsx
+++ src/Componant/Inspect Car/InspectCar.stories.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import InspectCar from "./InspectCar";
 
-export default{
-    titile:"Componant/Inspect Car/InspectCar",
-    Componant:InspectCar,
-}
-export const Default=()=><InspectCar/>
+export default {
+    title: "Component/Inspect Car/InspectCar", // Fixed 'titile' to 'title'
+    component: InspectCar, // Fixed 'Componant' to 'component'
+    parameters: {
+        docs: {
+            description: {
+                component: "This component is used to inspect car details visually and interactively.",
+            },
+        },
+    },
+};
+
+export const Default = () => <InspectCar />;
