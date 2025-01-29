c08b154b0825a3c0f47d370630471df9b5daeeea
ManojGaonkar2005 <manojgaonkar2005h@gmail.com>
2025-01-29 21:53:30 +0530

    Deleted the UnWanted Folders and Files

--- src/Componant/Header-Copy/HCop.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import HCopy from "./HCopy";
-
-export default{
-    titile:"Componant/Header-Copy/HCopy",
-    Componant:HCopy,
-}
-
-export const Default=()=><HCopy/>
--- src/Componant/Header/Header.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Header from "./header";
-
-export default{
-    titile:"Componant/Header/Header",
-    Componant:Header,
-};
- export const Default=()=><Header/>
