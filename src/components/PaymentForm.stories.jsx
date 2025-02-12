04454411a13ee043d4ee1bf7320a997099e0d3e8
ManojGaonkar2005 <manojgaonkar2005h@gmail.com>
2025-02-12 19:47:36 +0530

    Update in code

---
+++ src/components/PaymentForm.stories.jsx
@@ -0,0 +1,9 @@
+import PaymentForm from "./PaymentForm";
+import PaymentSummary from "./PaymentSummaryjsx";
+import React from "react";
+
+export default{
+  title:"Application/PaymentForm",
+  Application:PaymentForm
+}
+export const Default=()=><PaymentForm/>
