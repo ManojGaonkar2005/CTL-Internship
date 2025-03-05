490ef438677b21a7b16eadb76acf743b2def0772
ManojGaonkar2005 <manojgaonkar2005h@gmail.com>
2025-03-05 18:29:36 +0530

    Update

--- src/Component/Wearup/Wearup.jsx
+++ src/Component/Wearup/Wearup.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
+// import React from 'react';
 
-import './wearup.css';  
-import OrderSummary from './components/OrderSummary/OrderSummary';
-import Header from './components/Header/Header';
-import PaymentForm from './components/PaymentForm/PaymentForm';
+// import './wearup.css';  
+// import OrderSummary from './components/OrderSummary/OrderSummary';
+// import Header from './components/Header/Header';
+// import PaymentForm from './components/PaymentForm/PaymentForm';
 
- const Layout=()=> {
-  return (
-    <div className="app">
-      <div className="row1">
-        <Header/>
-      </div>
-      <div className="row2">
-        <div className="sub1">
-          <OrderSummary/>
-        </div>
-        <div className="sub2">
-          <PaymentForm/>
-        </div>
-      </div>
-    </div>
-  );
-}
+//  const Layout=()=> {
+//   return (
+//     <div className="app">
+//       <div className="row1">
+//         <Header/>
+//       </div>
+//       <div className="row2">
+//         <div className="sub1">
+//           <OrderSummary/>
+//         </div>
+//         <div className="sub2">
+//           <PaymentForm/>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Layout;
+// export default Layout;
